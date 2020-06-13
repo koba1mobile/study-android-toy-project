1135dca121c9c20569954ebc122bf2298b5430f9
koba <koba1mobile@gmail.com>
2020-06-13 22:07:00 +0900

    base commit - fab 클릭시 검색 프래그먼트로 이동

---
+++ koba1mobile/ToyProject/app/src/main/java/com/example/toyproject/HistoryFragment.kt
@@ -0,0 +1,35 @@
+package com.example.toyproject
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import kotlinx.android.synthetic.main.fragment_history.*
+
+/**
+ * A simple [Fragment] subclass as the default destination in the navigation.
+ */
+class HistoryFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_history, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        initView()
+    }
+
+    private fun initView() {
+        fab.setOnClickListener { view ->
+            findNavController().navigate(R.id.action_HistoryFragment_to_SearchFragment)
+        }
+    }
+}
---
+++ koba1mobile/ToyProject/app/src/main/java/com/example/toyproject/SearchFragment.kt
@@ -0,0 +1,25 @@
+package com.example.toyproject
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+
+/**
+ * A simple [Fragment] subclass as the second destination in the navigation.
+ */
+class SearchFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_search, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+}
